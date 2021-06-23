efb4395eef547b4d89ed8e79f084a97ee92ce362
Evgeny_Bobkov <freeedayy@gmail.com>
2021-06-23 17:08:13 +1000

    (5 kyu) Moving Zeros to the End (took 1.5 hours)

--- app.js
+++ app.js
@@ -34,3 +34,37 @@ function solution(string) {
 
 console.log(solution("camelCasingString"));
 */
+
+// (6 KYU) Format a string of names like 'Bart, Lisa & Maggie'.
+/*
+function list(names) {
+	const allNames = names.map((name) => name.name);
+	return names.length > 2
+		? `${allNames.slice(0, names.length - 1).join(", ")} & ${allNames.slice(
+				names.length - 1
+		  )}`
+		: allNames.join(" & ");
+}
+
+console.log(list([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }]));
+console.log(list([{ name: "Bart" }, { name: "Lisa" }]));
+*/
+
+// (5 KYU) Moving Zeros To The End
+
+const moveZeros = function (arr) {
+	const zeroArr = [];
+	const numsArr = [];
+
+	for (let i of arr) {
+		if (i === 0 && i !== false) zeroArr.push(i);
+		else numsArr.push(i);
+	}
+
+	return numsArr.concat(zeroArr);
+};
+
+//prettier-ignore
+console.log(
+	moveZeros(["a","b","c","d",1,false,"a","l","s","e",1,3,1,9,"[","o","b","j","e","c","t",0,"O","b","j","e","c","t","]",9,0,0,0,0,0,0,0,0,0,0,])
+);

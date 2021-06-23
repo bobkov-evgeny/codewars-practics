12feba6c56028222f410ed37c31d9030e30bfd0f
Evgeny_Bobkov <freeedayy@gmail.com>
2021-06-23 14:01:54 +1000

    (5 KYU) Extract the domain name from a URL

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// (5 KYU) Extract the domain name from a URL
+
+function domainName(url) {
+	const httpPart = url.split("//");
+	let currentUrl = "";
+	let result = "";
+
+	httpPart.length > 1
+		? (currentUrl = String(httpPart.slice(1)).split("."))
+		: (currentUrl = String(httpPart).split("."));
+
+	if (currentUrl[0] !== "www") result = currentUrl[0];
+	else result = currentUrl.slice(1, 2);
+
+	return String(result);
+}
+
+domainName("www.xakep.ru");
